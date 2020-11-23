453a29c9f969d4129081cbe2ca7e2aee727fdcbd
george-postolache <jumper.space@yahoo.com>
2020-11-23 16:52:45 +0200

    Ready

--- app/Models/Providers.php
+++ app/Models/Providers.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Providers extends Model
 {
     use HasFactory;
-    use SoftDeletes();
+    use SoftDeletes;
     protected $dates = ['deleted_at'];
 }
